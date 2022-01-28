376f5db9ed59d5096b9580469348e3060ebc8234
Cazaubon <ccolin333@gmail.com>
2022-01-28 18:56:34 +0100

    Spring boot initialization

---
+++ api/src/test/java/com/boatapp/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boatapp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
